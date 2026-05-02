160652978a84d8ceb383b28ca182a9f63c06cb9a
Batbold-270894331 <270894331@yoobeestudent.ac.nz>
2026-05-02 22:05:50 +1200

    Update Week3&4

--- Week3/main.py
+++
@@ -1,9 +0,0 @@
-from database import create_tables
-from user_manager import add_user, view_users, search_user, delete_user
-
-def main():
-    create_tables()
-    print("All tables created.")
-
-if __name__ == "__main__":
-    main()
