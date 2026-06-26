cc28a5f22351f640ccfee394663e21ee6c3e5252
Batbold-270894331 <270894331@yoobeestudent.ac.nz>
2026-06-27 09:59:29 +1200

    add more functions on Week12 - Flask

--- Week12/hello_flask.py
+++ Week12/hello_flask.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Flask!"
+    return "<div><p>Hello, Flask!</p></div>"
 
 @app.route('/bye')
 def bye():
@@ -13,6 +13,14 @@ def bye():
 @app.route('/user/<username>')
 def user(username):
     return f"Hello, {username}!"
+
+@app.route('/user/<username>/<int:age>')
+def user_with_age(username, age):
+    return f"Hello, {username}! You are {age} years old."
+
+@app.route("/flask")
+def home():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
